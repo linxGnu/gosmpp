d857c9e989d798223132ade147aa62cdecb5a280
linxGnu <linhduonggnu@gmail.com>
2019-12-26 16:59:23 +0900

    NO-ISSUE Invoker is now able to find suitable encoding for message

---
+++ data/utils.go
@@ -0,0 +1,23 @@
+package data
+
+import "unicode"
+
+// FindEncoding returns suitable encoding for a string.
+// If string is ascii, then GSM7Bit. If not, then UCS2.
+func FindEncoding(s string) (enc Encoding) {
+	if isASCII(s) {
+		enc = GSM7BIT
+	} else {
+		enc = UCS2
+	}
+	return
+}
+
+func isASCII(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] > unicode.MaxASCII {
+			return false
+		}
+	}
+	return true
+}
---
+++ data/utils_test.go
@@ -0,0 +1,12 @@
+package data
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFindEncoding(t *testing.T) {
+	require.Equal(t, GSM7BIT, FindEncoding("abc30hb3bk2lopzSD=2-^"))
+	require.Equal(t, UCS2, FindEncoding("Trần Lập và ban nhạc Bức tường huyền thoại"))
+}
