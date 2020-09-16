3243bd0ff94e9225c715d24971ddd72384512fa3
Septs <github@septs.pw>
2020-09-16 21:42:19 +0800

    add comment

--- types.go
+++ types.go
@@ -2,10 +2,14 @@ package gosmpp
 
 import "github.com/linxGnu/gosmpp/pdu"
 
+// PDUCallback handles received PDU.
 type PDUCallback func(pdu pdu.PDU, responded bool)
 
+// PDUErrorCallback notifies fail-to-submit PDU with along error.
 type PDUErrorCallback func(pdu pdu.PDU, err error)
 
+// ErrorCallback notifies happened error while reading PDU.
 type ErrorCallback func(error)
 
+// ClosedCallback notifies `closed` event due to State.
 type ClosedCallback func(State)
